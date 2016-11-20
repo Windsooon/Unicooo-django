f8e4997aff98856d6629edabb4da803e46946d25
WindsonYang <wiwindson@123.com>
2016-11-20 20:16:56 +0800

    change directory

--- shell/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-UNI=/Users/windson/uni/srv/unicooo/www
-cd $UNI
-python3 manage.py runserver --settings=unicooo.settings.local
--- shell/test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-UNI=/Users/windson/uni/srv/unicooo/www
-cd $UNI
-python3 manage.py test --settings=unicooo.settings.local
