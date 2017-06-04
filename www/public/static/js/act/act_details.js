1c8ddf9393a9f34104d065774c4889d4b4a2df7c
Windsooon <wiwindson@outlook.com>
2017-06-05 01:31:18 +0800

    add details feature

---
+++ www/public/static/js/act/act_details.js
@@ -0,0 +1,30 @@
+var $act_edit_btn = $("#act-edit-btn");
+$act_edit_btn.on("click", function() {
+    if ($act_edit_btn.text() == "Edit") {
+        var editor = $("#act-editor").data('quill');
+        editor.enable();
+        $act_edit_btn.text("Save");
+        $(".ql-toolbar").css("display", "block");
+    }
+    else if ($act_edit_btn.text() == "Save") {
+        var editor = $("#act-editor").data('quill');
+        var delta = editor.getContents();
+        var act_id = $(".activity-details-content").children('input').val();
+        var csrftoken = getCookie('csrftoken');
+        $.ajax({
+            url: "/api/acts/" + act_id + "/",
+            type: "PATCH",
+            beforeSend: function(xhr) {
+                xhr.setRequestHeader('X-CSRFToken', csrftoken)
+            },
+            dataType: 'json',
+            data: {
+                csrfmiddlewaretoken: csrftoken,
+                "act_intro": JSON.stringify(delta)},
+            success: function(data) {
+                $act_edit_btn.text("Edit");
+                $(".ql-toolbar").css("display", "none");
+            },
+        });
+    }
+});
