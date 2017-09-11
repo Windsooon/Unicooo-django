24bb413f518c4196a14da24f909b02fb35a0ce33
Windsooon <wiwindson@outlook.com>
2017-09-11 23:42:25 +0800

    add cache when create post and act

--- www/common/signals.py
+++ www/common/signals.py
@@ -2,6 +2,8 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.core.cache import cache
 from .models import MyUser
+from post.models import Post
+from activities.models import Act
 
 
 @receiver(post_save, sender=MyUser)
@@ -10,3 +12,15 @@ def init_user(sender, **kwargs):
     cache.set("email_" + user.email, 1, timeout=None)
     cache.set("user_name_" + user.user_name, 1, timeout=None)
     cache.set("user_points_" + str(user.id), 50, timeout=None)
+
+
+@receiver(post_save, sender=Post)
+def init_post(sender, **kwargs):
+    post = kwargs['instance']
+    cache.set("post_"+str(post.id), 0, timeout=None)
+
+
+@receiver(post_save, sender=Act)
+def init_act(sender, **kwargs):
+    act = kwargs['instance']
+    cache.set("act_"+str(act.id), 0, timeout=None)
